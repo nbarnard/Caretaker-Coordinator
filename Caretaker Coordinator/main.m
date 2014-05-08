51955c99c71082f972ee83bc6d71eee677b27e0d
Nicholas Barnard <github-298f98981b@nickb.users.panix.com>
2014-05-07 23:26:54 -0700

    Initial Commit

---
+++ Caretaker Coordinator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Caretaker Coordinator
+//
+//  Created by Nicholas Barnard on 5/7/14.
+//  Copyright (c) 2014 NMFF Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NCBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NCBAppDelegate class]));
+    }
+}
---
+++ Caretaker CoordinatorTests/Caretaker_CoordinatorTests.m
@@ -0,0 +1,34 @@
+//
+//  Caretaker_CoordinatorTests.m
+//  Caretaker CoordinatorTests
+//
+//  Created by Nicholas Barnard on 5/7/14.
+//  Copyright (c) 2014 NMFF Development. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Caretaker_CoordinatorTests : XCTestCase
+
+@end
+
+@implementation Caretaker_CoordinatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
